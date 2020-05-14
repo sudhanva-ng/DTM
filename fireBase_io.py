e17cfbabed9dc0c69ca8a3dc5d309e68460f96bb
sudng <sudng@cisco.com>
2020-05-14 12:23:41 +0530

    uppdated firebase_io

--- fireBase_io.py
+++ fireBase_io.py
@@ -8,7 +8,7 @@ class db:
 		self.handler = firebase.FirebaseApplication(url, None)
 
 	def write(self,dest,data):
-		result = self.handler.post(dest,data)
+		result = self.handler.patch(dest,data)
 		return result
 
 
@@ -17,9 +17,15 @@ class db:
 		return (result)
 
 # mydb = db('https://debunk-the-myths.firebaseio.com/')
-# result = mydb.read('/test/-M6GZph798ThetACfTBN/Members/1/','Name')
+# # result = mydb.read('/test/-M6GZph798ThetACfTBN/Members/1/','Name')
 
-# data = {'Members':[{'Name':'Sudhanva','cec':'sudng'},{'Name':'Nikitha','cec':'ninagell'}]}
+# # data = {'Members':['Name':'Sudhanva','cec':'sudng'},{'Name':'Nikitha','cec':'ninagell'}]}
+
+# #data = {'https://ichef.sudng s :s:':['Name','https://ichef.sudng s :s:','test']}
+
+# data = {'Bbc-com_2020-05-11T23_00_15Z': ['Safoora Zargar: Why did India jail a pregnant student during Covid-19?', 'https://www.bbc.com/news/world-asia-india-52608589', 'https://ichef.bbci.co.uk/news/1024/branded_news/14066/production/_112222028_photo-2020-05-04-12-17-38.jpg']}
+# # print(type(data))
+
 # result = mydb.write('/test/test1',data)
 
 # print(result)
